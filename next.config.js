924010feb6d373f8fca3654e33e45338ddcff512
Karim Aljandali <karimaljandali@gmail.com>
2022-05-15 17:51:02 -0400

    Production fix for faulty Atom/Nextjs issue

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+// https://github.com/facebookexperimental/Recoil/issues/733#issuecomment-925072943
+// Temporary production fix for Recoil warning message
+const intercept = require("intercept-stdout")
+
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return ''
+  }
+  return text
+}
+
+// Intercept in dev and prod
+intercept(interceptStdout)
+
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,33 @@ import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import { RecoilRoot } from 'recoil'
 
+
+// https://github.com/facebookexperimental/Recoil/issues/733#issuecomment-925072943
+// Temporary production fix for Recoil warning message
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+const mutedConsole = memoize((console) => ({
+  ...console,
+  warn: (...args) => args[0].includes('Duplicate atom key')
+    ? null
+    : console.warn(...args)
+}))
+
+global.console = mutedConsole(global.console);
+
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
     <SessionProvider session={session}>
